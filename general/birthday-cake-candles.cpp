3ce9c2de7536748fa38c5e2e6057b6bf1db838bd
ibrkhalil <vampirekid017@gmail.com>
2024-06-01 23:49:27 +0300

    Happy birthday

---
+++ general/birthday-cake-candles.cpp
@@ -0,0 +1,25 @@
+// Hurdle Race
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int highestCandle = -1, occurrences = 0;
+    for (int num:arr) {
+        if (num > highestCandle) {
+            highestCandle = num;
+        }
+        if (num == highestCandle) {
+            ++occurrences;
+        }
+    }
+
+    cout << occurrences;
+    
+}
