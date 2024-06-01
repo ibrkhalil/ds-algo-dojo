dd78b95f0f6350a35ff037b9dc0bb9cfd9176af8
ibrkhalil <vampirekid017@gmail.com>
2024-06-01 23:37:00 +0300

    A very big sum

---
+++ general/a-very-big-sum.cpp
@@ -0,0 +1,17 @@
+// Hurdle Race
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    long long x;
+    unsigned long long sum = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        sum += x;
+    }
+    cout << sum;
+    
+}
