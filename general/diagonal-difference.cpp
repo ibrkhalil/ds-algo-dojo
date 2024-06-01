9f2e0c477eab478efa73bea71345ae7bfa70a062
ibrkhalil <vampirekid017@gmail.com>
2024-06-02 00:57:20 +0300

    Diagonal differecne

---
+++ general/diagonal-difference.cpp
@@ -0,0 +1,31 @@
+// Diagonal difference
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int matrix[n][n];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int sumLeftDiag = 0, sumRightDiag = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j) {
+                sumLeftDiag += matrix[i][j];
+            }
+            if ((i + j) == n-1) {
+                sumRightDiag += matrix[i][j];
+            }
+        }
+    }
+    cout << abs(sumRightDiag - sumLeftDiag);
+
+}
