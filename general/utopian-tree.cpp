cd22245b3efd531359b2bd35815f98d2ef168a9e
ibrkhalil <vampirekid017@gmail.com>
2024-06-02 00:10:51 +0300

    utopia

---
+++ general/utopian-tree.cpp
@@ -0,0 +1,32 @@
+// Utopian tree
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == 0) {
+            cout << 1 << "\n";
+            continue;
+        } else if (arr[i] == 1) {
+            cout << 2 << "\n";
+            continue;
+        }
+        int val = 1;
+        for (int i = 0; i < arr[i]; ++i) {
+            if (i % 2 == 0) {
+                val++;
+            } else val+=val;
+        }
+        cout << val << "\n";
+    }
+    
+    
+}
