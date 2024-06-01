3e840e0d7c7c4b843e5d39db1e7712e15fc469c8
ibrkhalil <vampirekid017@gmail.com>
2024-06-01 23:17:07 +0300

    Hurdle race

---
+++ general/hurdle-race.cpp
@@ -0,0 +1,21 @@
+// Hurdle Race
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int highestHurdle = -1;
+    for (int num:arr) {
+        if (num > highestHurdle) {
+            highestHurdle = num;
+        }
+    }
+    cout << max(0, highestHurdle - k);
+    
+}
