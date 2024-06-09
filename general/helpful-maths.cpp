4ded3212edb79e0430530af404c4d16e1d908b02
ibrkhalil <vampirekid017@gmail.com>
2024-06-09 15:18:13 +0300

    Helpful maths

---
+++ general/helpful-maths.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int strLen = s.length();
+    vector<string> vect;
+    for (int i = 0; i < strLen; i++)
+    {   
+        if(s[i] == '1' || s[i] == '2' || s[i] == '3') {
+        vect.push_back(string(1, s[i]));
+        }
+    }
+    sort( vect.begin(), vect.end() );
+    int size = vect.size();
+    string resString;
+    for (int i = 0; i < size; ++i) {
+        string plus = i == size - 1 ? "" : "+";
+        resString += vect[i] + plus;
+    }
+    cout << resString;
+}
